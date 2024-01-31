a7ae8255d44b49c49fb9f223e6dd0751a5c3dd1f
RahmaButt <rahmabutt545.com>
2024-01-31 17:28:38 +0500

    comment

--- login/urls.py
+++ login/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("loginpage.urls"))
+    path("", include("loginpage.urls")),
+     path('', include('django.views.generic.base.RedirectView'), redirect_to='/404/')
 ]
--- loginpage/urls.py
+++ loginpage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import NotFoundView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name="signup-page"),
     path('login', views.loginPage, name="login-page"),
-    path('home', views.homePage, name="home-page")
-    
+    path('home', views.homePage, name="home-page"),
+    path('404/', NotFoundView.as_view()),
 ]
